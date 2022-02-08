9958f9fce811bb263585288b9e8da128e9527230
melissasjostedt <97900446+melissasjostedt@users.noreply.github.com>
2022-02-08 11:24:45 +0100

    Update index.js

--- AveragerApp/index.js
+++ AveragerApp/index.js
@@ -4,7 +4,7 @@
 // Don't forget to include your CSS-file in the folder
 
 // Include the link to your Github Repository here:
-// Link: https://github.com/mybergquistrajic/me132a-U1.git
+// Link: https://github.com/melissasjostedt/Me132A-uppgif1-Melissa.Sjostedt
 
 
 
